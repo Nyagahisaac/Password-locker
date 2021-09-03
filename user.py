7b4d65f9e0e361d9ca2b8d575da60829295fbc0e
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-04 01:54:10 +0300

    define the __init__ method with its parameter

--- user.py
+++ user.py
@@ -4,4 +4,12 @@ class User:
     '''
     pass
 
-   
+    def __init____(self,username,password):
+        '''
+        __init__ method helps us define properties for our objects.
+        
+        Args:
+        username : New user username.
+        password :New user password.
+        '''
+       
