26acd92d773793e70c2e6e12694719dae40eecb7
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-04 02:43:07 +0300

    import the unittest and user from the user class to user_test.py file

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest 
+from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class  defies test cases for the user class behaviours
+    
+    Args:
+    unittest.Testcase:Test case that helps in creating test cases
+    '''
+    
