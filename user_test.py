20506d9bc17e555ac03d9b03c9b9d93995528c2b
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-04 02:48:49 +0300

    add the set up and test initiliser method

--- user_test.py
+++ user_test.py
@@ -9,3 +9,13 @@ class TestUser(unittest.TestCase):
     unittest.Testcase:Test case that helps in creating test cases
     '''
     
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Izooh The Duke","124vg3 sdx")
+        
+    def test_init_(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
