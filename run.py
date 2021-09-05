8ba4d2b9b230daad94acecb729f04d777b9bd186
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-05 12:19:44 +0300

    add display user method

--- run.py
+++ run.py
@@ -31,3 +31,10 @@ def check_existing_users(username):
     Function that check if user exists with that username and return a Boolean
     '''
     return User.user_exist(username)
+
+def display_users():
+    '''
+    Function that returns all the saved users
+    '''
+    return User.display_users()
+
