e7db52770ce737bcab8e1e7d171f5ba16f366ab1
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-05 12:07:44 +0300

    create new user in run.py to implement our behaviours

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3.8
+from user import User
+
+def create_user(newusername,newpassword):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(newusername,newpassword)
+    return new_user
